b7ebc82218b0f6dfb3989f1d3e136f65cdec625b
tsotne latsabidze <tsotnelatsabidze@outlook.com>
2023-11-15 15:48:50 +0400

    deleted globasSuppressions

--- TodoListApp.Services.WebApi/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage(
-    "Minor Code Smell",
-    "S1075:URIs should not be hardcoded",
-    Justification = "The URI is a constant in this context and does not need to be configurable.",
-    Scope = "member",
-    Target = "~M:TodoListApp.Services.WebApi.TodoListWebApiService.#ctor")]
